6d42b876139da0eb599f8d521a546ec329636bed
aly <alyssonrosa1@gmail.com>
2022-06-25 13:56:01 -0300

    base

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import TodoList from "./components/TodoList.vue";
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  </header>
+
+  <main>
+    <todo-list></todo-list>
+  </main>
+</template>
+
+<style>
+@import './assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="screen">
+        <div class="todo">
+            <input type="text" class="add">
+        </div>
+        <div>
+            <div class="item" v-for="todo in todos">
+                {{ todo.text }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    data() {
+        return {
+            todos: [
+                {
+                    id: 1,
+                    text: "Learn Vue",
+                    done: false
+                },
+                {
+                    id: 2,
+                    text: "Learn TypeScript",
+                    done: false
+                },
+                {
+                    id: 3,
+                    text: "Learn Vuex",
+                    done: false
+                }
+            ]
+        }
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+#screen {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
+
+    font-weight: normal;
+}
+
+.todo {
+    display: flex;
+    justify-content: center;
+
+    .add {
+        transition: ease-in-out 0.2s;
+        width: 350px;
+        border-radius: 5px;
+        border: none;
+        height: 25px;
+        text-align: center;
+        background-color: #3F3F3F;
+        color: #fff;
+    }
+}
+</style>
